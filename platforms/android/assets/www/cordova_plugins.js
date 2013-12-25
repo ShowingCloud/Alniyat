0aa7d7d976cb6a1c983b473c79c5b61306a0fd0b
Yu Jin <kingfish86@163.com>
2013-12-25 14:32:50 +0800

    update the nfc project example: change the login dialog

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,10 +6,38 @@ module.exports = [
         "runs": true
     },
     {
+        "file": "plugins/com.chariotsolutions.toast.plugin/www/phonegap-toast.js",
+        "id": "com.chariotsolutions.toast.plugin.Toasty",
+        "clobbers": [
+            "toast"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.battery-status/www/battery.js",
         "id": "org.apache.cordova.battery-status.battery",
         "clobbers": [
             "navigator.battery"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification"
         ]
     }
 ]
